e032b49ea26b1a7a193dff086398fa2e5625130b
Devansh Maurya <Devansh233@gmail.com>
2018-11-15 10:15:09 +0530

    Final Arduino code

--- bluetooth/bluetooth.ino
+++ bluetooth/bluetooth.ino
@@ -5,12 +5,15 @@ String readdata;
 char data = 0;            //Variable for storing received data
 bool ledOn = false;
 int startDelay, stopAfter;
+char c;
+int bulb_1 = 0, bulb_2 = 0;
 
 void setup()
 {
     BT.begin(38400);
     Serial.begin(9600);   //Sets the baud for serial data transmission                               
-    pinMode(PIN, OUTPUT);  //Sets digital pin 13 as output pin
+    pinMode(3, OUTPUT);  //Sets digital pin 13 as output pin
+    pinMode(2, OUTPUT);
 }
 
 void loop()
@@ -22,7 +25,6 @@ void loop()
       readdata+=data;
    }
 
-   int bulb_1, bulb_2;
    //Read the incoming data & store into data
    if(readdata.length()>0)
    {
@@ -30,43 +32,69 @@ void loop()
       Serial.print("\n");   
 
       if(readdata == "1") {
-        bulb_1 = readdata.toInt();
-        bulb_2 = 0;
+        bulb_1 = 3;
+        digitalWrite(bulb_1, LOW);
+        readdata = "";
       }
 
-      if(readdata == "2") {
-        bulb_1 = 0;
-        bulb_2 = readdata.toInt();
-      }
-
-      if(readdata == "12") {
-        bulb_1 = 1;
+      else if(readdata == "2") {
         bulb_2 = 2;
+        digitalWrite(bulb_2, LOW);
+        readdata = "";
+      }
+      
+      else if(readdata == "01") {
+        bulb_1 = 0;
+        digitalWrite(bulb_1, LOW);
+        readdata = "";
+      }
+
+      else if(readdata == "02") {
+        bulb_2 = 0;
+        digitalWrite(bulb_2, LOW);
+        readdata = "";
+      }
+      
+      if(readdata == "STOP") {
+        bulb_1 = 0;
+        bulb_2 = 0;
+        readdata = "";
+        digitalWrite(bulb_1, LOW);
+        digitalWrite(bulb_2, LOW);
       }
            
-      if(readdata == "ON") {             // Checks whether value of data is equal to 1
-         analogWrite(PIN, 255);
+      if(readdata == "ON") {        
+         digitalWrite(bulb_2, HIGH);
+         digitalWrite(bulb_1, HIGH);
          readdata = "";
          ledOn = true;
       }
-      else if(readdata == "OFF") {         //  Checks whether value of data is equal to 0
-         analogWrite(PIN, 0);    //If value is 0 then LED turns OFF
+      else if(readdata == "OFF") {
+         digitalWrite(bulb_2, LOW);
+         digitalWrite(bulb_1, LOW);
          readdata = "";
          ledOn = false;
       }
-      else if(readdata != "") {
-        if(readdata[0] == 'b') {
-          startDelay = readdata.substring(1, 4).toInt();
-          stopAfter = readdata.substring(6, 9).toInt();
-          delay(startDelay);
-          analogWrite(PIN, (readdata.substring(10).toInt()));  
-          delay(stopAfter);
-          analogWrite(PIN, 0);
+      else if(readdata[0] == 'b') {
+        //Get index of 'e', number following 'e' indicates the number of seconds for which the bulb(s) should remain on
+        c = readdata.indexOf('e');    
+        
+        startDelay = readdata.substring(1, c-1).toInt();
+        stopAfter = readdata.substring(c+1, readdata.length()-1).toInt();
+
+        if(startDelay != 0) {
+          delay(startDelay*10);
+          digitalWrite(bulb_1, HIGH);
+          digitalWrite(bulb_2, HIGH);
         }
-        else
-          analogWrite(PIN, readdata.toInt());
+
+        if(stopAfter != 0) {
+          delay(stopAfter*10);
+          digitalWrite(bulb_1, LOW);
+          digitalWrite(bulb_2, LOW);
+        }
         readdata="";
-        ledOn = true;
       }
+      ledOn = true;
    }
 }
